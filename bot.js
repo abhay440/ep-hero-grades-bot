3a18db72763c2707be9f76212a6c81866d94cfe3
Abhay Kore <akore@crsinc.com>
2019-02-24 18:39:37 -0500

    Added logs

--- bot.js
+++ bot.js
@@ -34,11 +34,18 @@ client.on('guildMemberAdd', member => {
 })
 
 client.on('message', message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return
+  log(`Received message: ${message}`)
+  if (!message.content.startsWith(prefix) || message.author.bot) {
+    log('Invalid content')
+    return
+  }
   const args = message.content.slice(prefix.length).split(/ +/)
   const commandName = args.shift().toLowerCase()
   const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-  if (!command) return
+  if (!command) {
+    log(`Command not found: ${command}`)
+    return
+  }
   try {
     log(`Executing command ${command.name.toUpperCase()} initiated from server: ${message.guild.name.toUpperCase()} in channel: ${message.channel.name.toUpperCase()}`)
     command.execute(message, args)
