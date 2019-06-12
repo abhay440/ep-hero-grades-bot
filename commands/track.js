e84f453b9b85c7820d54a0fd7ba8f4cd81bf1df2
Abhay Kore <akore@crsinc.com>
2019-06-12 14:14:16 -0400

    Added new chart support

--- commands/track.js
+++ commands/track.js
@@ -164,6 +164,21 @@ module.exports = {
 			});
     	}
 
+        function decodeBase64Image(dataString) {
+          var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+          var response = {};
+
+          if (matches.length !== 3) 
+          {
+            return new Error('Invalid input string');
+          }
+
+          response.type = matches[1];
+          response.data = new Buffer(matches[2], 'base64');
+
+          return response;
+        }
+
 		function ocr() {
 			return new Promise ((resolve, reject) => {
 
