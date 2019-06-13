dc1a7843b35e9fad9183cd5334da521ba4495e75
Abhay Kore <akore@crsinc.com>
2019-06-13 14:51:33 -0400

    Fixes

--- commands/track.js
+++ commands/track.js
@@ -111,67 +111,33 @@ module.exports = {
     	function sendToGoogleSheets(parsedText, alliance, stars, titan) {
     		log("Sending to Google");
     		return new Promise ((resolve, reject) => {
-                var form = {
-                    data: parsedText,
-                    stars: stars,
-                    titan: titan,
-                    alliance: alliance
-                };
+          var form = {
+              data: parsedText,
+              stars: stars,
+              titan: titan,
+              alliance: alliance
+          };
 
-				var url;
-				var chartUrl;
-
-    			if (alliance === 'panda') {
-					url = 'https://script.google.com/macros/s/AKfycbzCKDrvrdm_IRZdZTtDkLeGCMkohZS9OEW0EDQHPuoR-VEz4yA/exec'; //panda
-					chartUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSXFu4o3U9oLIU0XVLOlSS9RdX6UiaAHhYOqOcUalQ21oKgo_XsnToHUz5wQ-UndyjuxrZL2XQPPPhJ/pubchart?oid=649661949&format=image';
-    			}
-    			else if (alliance === 'cub') {
-					url = 'https://script.google.com/macros/s/AKfycbxvpByOGedpNy8zjBWnLYaH8_q3ik-z2yifgLPD/exec'; //cub
-					chartUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vScHKxJWYuWW2878Cjfj5geN-mWsiMsqMXadM2fT0I_yNQIJw5_fQAX96BMiSkxjykMhDBxojKD-k7b/pubchart?oid=649661949&format=image';
-    			}
-                else if (alliance === 'knight') {
-                    //chartUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSHNzZzaP37muvIXpL4pHveTbTFFPsQfcciYJ3uSEBN2WrI0HR-C3vkYnrYCTwUh8NLJ7pww0BaLV6Z/pubchart?oid=581420534&format=image';
-                    // Send to new sheet
-                    var newUrl = 'https://script.google.com/macros/s/AKfycbw_yKkDmU5AKesbJWIiZXxdy8AVRpNEQzr4ZngQd6PHlqJj0hBW/exec';
-                    var r = request.post({ url: newUrl, followAllRedirects: true, form: form }, function optionalCallback (err, httpResponse, body) {
-                      if (err) {
-                        log('upload failed:' + err);
-                        reject(err);
-                        return;
-                      }
-                      log(`Success: sent to new Google sheet`);
-                      var jsonBody = JSON.parse(body);
-                      if (jsonBody.error === true) {
-                        reject(jsonBody.errorMessage);
-                        return;
-                      } else {
-                        var imageBuffer = decodeBase64Image(jsonBody.chart.image);
-                        resolve(imageBuffer.data);
-                      }
-                    });
-                    return;
-                } else {
-    				reject("Invalid Alliance");
-    				return;
-    			}
-
-                chartUrl = chartUrl + "&time=" + Math.floor(Date.now() / 1000);
-
-				//var url = 'https://script.google.com/macros/s/AKfycbzCKDrvrdm_IRZdZTtDkLeGCMkohZS9OEW0EDQHPuoR-VEz4yA/exec'; //panda
-				//var url = 'https://script.google.com/macros/s/AKfycbxvpByOGedpNy8zjBWnLYaH8_q3ik-z2yifgLPD/exec'; //cub
-
-				// pandachart = https://docs.google.com/spreadsheets/d/e/2PACX-1vSXFu4o3U9oLIU0XVLOlSS9RdX6UiaAHhYOqOcUalQ21oKgo_XsnToHUz5wQ-UndyjuxrZL2XQPPPhJ/pubchart?oid=649661949&format=image
-				// cubchart = https://docs.google.com/spreadsheets/d/e/2PACX-1vScHKxJWYuWW2878Cjfj5geN-mWsiMsqMXadM2fT0I_yNQIJw5_fQAX96BMiSkxjykMhDBxojKD-k7b/pubchart?oid=649661949&format=image
-
-				var r = request.post({ url: url, form: form }, function optionalCallback (err, httpResponse, body) {
-				  if (err) {
-				  	log('upload failed:' + err);
-					reject(err);
-				  }
-				  log(`Success: sent to Google`);
-				  resolve(chartUrl);
-				});
-			});
+          // Send to new sheet
+          var newUrl = 'https://script.google.com/macros/s/AKfycbw_yKkDmU5AKesbJWIiZXxdy8AVRpNEQzr4ZngQd6PHlqJj0hBW/exec';
+          var r = request.post({ url: newUrl, followAllRedirects: true, form: form }, function optionalCallback (err, httpResponse, body) {
+            if (err) {
+              log('upload failed:' + err);
+              reject(err);
+              return;
+            }
+            log(`Success: sent to new Google sheet`);
+            var jsonBody = JSON.parse(body);
+            if (jsonBody.error === true) {
+              reject(jsonBody.errorMessage);
+              return;
+            } else {
+              var imageBuffer = decodeBase64Image(jsonBody.chart.image);
+              resolve(imageBuffer.data);
+            }
+          });
+          return;
+        });
     	}
 
         function decodeBase64Image(dataString) {
