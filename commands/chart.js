946553fe1c2e3cafb35df363b5eb5fb6a52263b8
Abhay Kore <akore@crsinc.com>
2019-10-21 20:07:04 -0400

    Charts

--- commands/chart.js
+++ commands/chart.js
@@ -65,26 +65,11 @@ module.exports = {
                         reject(jsonBody.errorMessage);
                         return;
                     } else {
-                        var imageBuffer = decodeBase64Image(jsonBody.chart.image);
-                        resolve(imageBuffer.data);
+                        resolve(jsonBody.dashboardUrl);
                     }
                 });
                 return;
             });
-        }
-
-        function decodeBase64Image(dataString) {
-            var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-            var response = {};
-
-            if (matches.length !== 3) {
-                return new Error('Invalid input string');
-            }
-
-            response.type = matches[1];
-            response.data = new Buffer(matches[2], 'base64');
-
-            return response;
         }
     },
 }
